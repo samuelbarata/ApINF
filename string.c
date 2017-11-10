17fb54e12283bdf39ddfb8f31a8899f1fb16b2b7
samuelbarata <oliveira3@icloud.com>
2017-11-10 15:46:38 +0000

    edited

--- string.c
+++ string.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 main(){
-	char name[] = "Samuel";
+/*	char name[] = "Samuel";
 	char my_String[] = {'S', 'a', 'm', 'u', 'e', 'l', '\0'};
 	for(int i=0; i<6; i++){
 		printf("%c", name[i]);
@@ -11,4 +11,28 @@ main(){
 	
 	char str2[]={'B','o','m',' ','d','i','a', ' ', 'T','u','r','m','a','\0'};
 	printf("\n%s\n", str2);
+	
+	printf("\n%c\n", str2[2]);
+*/	
+/*	
+	char oi[] = "Bom dia turma";
+	printf("%s\n%c%c\n",oi ,oi[1], oi[5]);
+*/	
+
+
+
+
+
+	do{
+		char nome[] = "";
+		puts("Introduza um nome:");
+		//puts("");//tem um \n encorpurado #paragrafo
+		gets(nome);
+		if(nome[0] == 0){
+			return 0;
+		}
+		printf("O nome e: %s\n\n",nome);
+	}while(1!=0);
 }
+
+
